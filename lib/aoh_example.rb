16449e5516badecbc6a14a8fbcab12b2d5ec4ee0
Sarah Rose <sarahrose63089@gmail.com>
2020-06-05 15:31:47 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -28,7 +28,8 @@ def literal_aoh
     {name:  "Kate Gompert", occupation: "None" },
     {name:  "Bruce Green", occupation: "Fan of Mildred"}
   ]
-  
+  assembled.sort!
+  assembled
 end
 
 def aoh_lookup(aoh, row, key)
